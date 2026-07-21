eff0a82cc112656570f92d5e9d6e7618923e2eef
lisatir07 <l.tir2620@gmail.com>
2026-07-21 13:57:28 -0600

    Initial commit

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,13 @@
+print("EV Telemetry Dashboard Starting...")
+
+temperature = 30.5
+voltage = 12.4
+throttle = 75
+
+print("----------------------------")
+print("EV TELEMETRY")
+print("----------------------------")
+print(f"Throttle: {throttle}%")
+print(f"Temperature: {temperature} C")
+print(f"Battery Voltage: {voltage} V")
+print("----------------------------")
